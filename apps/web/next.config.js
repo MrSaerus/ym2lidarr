4f60117ed6f68f7a53b56d02d2aa465bb9bb9313
Максим Абдулхаликов <maksim.abdulhalikov@tages.ru>
2025-09-02 23:28:50 +0500

    Fix dockerfile and add docker compose

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const PORT_API = process.env.PORT_API || 'http://localhost:4000';
 const nextConfig = {
   output: 'standalone',
   eslint: { ignoreDuringBuilds: true },
   reactStrictMode: true,
+
+  async rewrites() {
+    return [{ source: '/:path*', destination: `${PORT_API}/:path*` }];
+  }
 };
+
 module.exports = nextConfig;
